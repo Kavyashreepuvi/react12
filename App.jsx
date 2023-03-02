27c59cd8e10ece271559ca728f2382dc44a32103
Kavyashreepuvi <120468531+Kavyashreepuvi@users.noreply.github.com>
2023-03-02 10:40:06 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const App=() =>{
+    return(
+        <>
+        <div> kelsa kodisro modallu</div>
+        <div>hello</div>
+        </>
+        
+    )
+        
+    
+}
+export default App
